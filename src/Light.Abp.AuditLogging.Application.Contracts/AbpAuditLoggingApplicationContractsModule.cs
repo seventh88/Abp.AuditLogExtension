87e13575db033399dcf677c3c0c7ebb0181ef923
seventh <panwei@mosainet.com>
2023-09-20 17:19:02 +0800

    upgrade to abp 7.3.0

--- src/Light.Abp.AuditLogging.Application.Contracts/AbpAuditLoggingApplicationContractsModule.cs
+++ src/Light.Abp.AuditLogging.Application.Contracts/AbpAuditLoggingApplicationContractsModule.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
 
 namespace Volo.Abp.AuditLogging
 {
-    [DependsOn(typeof(AbpAuditLoggingDomainSharedModule))]
+    [DependsOn(typeof(AbpAuditLoggingDomainSharedModule),
+        typeof(AbpMultiTenancyAbstractionsModule))]
     public class AbpAuditLoggingApplicationContractsModule : AbpModule
     {
     }
